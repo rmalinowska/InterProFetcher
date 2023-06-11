fe82d0df6d9a57cbc5139c02926c50e8d1467b0e
Roksana Malinowska <r.p.malinowska@gmail.com>
2023-06-11 11:23:04 +0200

    Add more functions

--- Bio/InterProFetcher/__init__.py
+++ Bio/InterProFetcher/__init__.py
@@ -214,7 +214,7 @@ def browse_by_type(type: str, keyword: str = "", write_on_sdout: bool = True, sa
     """
     Browse entries from the InterPro database based on a specific type and keyword.
 
-    Argument type is a type of entry to browse (eg. family, domain), keyword (optional argument) is a keyword used to filter the entries.
+    Argument type is a type of entry to browse (e.g., family, domain), keyword (optional argument) is a keyword used to filter the entries.
 
     Returns list of accession numbers of selected type that are matching the request.
     """
@@ -224,7 +224,6 @@ def browse_by_type(type: str, keyword: str = "", write_on_sdout: bool = True, sa
     
     else:
         BASE_URL = f"https://www.ebi.ac.uk:443/interpro/api/entry/InterPro/?type={type}&page_size=200"
-
 
     context = ssl._create_unverified_context()
 
@@ -243,8 +242,8 @@ def browse_by_type(type: str, keyword: str = "", write_on_sdout: bool = True, sa
             payload = json.loads(res.read().decode())
             next = payload["next"]
             attempts = 0
-            if not next:
-                last_page = True
+            if not next: #
+                last_page = True #
         except HTTPError as e:
             if e.code == 400:
                 sleep(61)
@@ -263,7 +262,7 @@ def browse_by_type(type: str, keyword: str = "", write_on_sdout: bool = True, sa
         for i, item in enumerate(payload["results"]):
             accesion = item["metadata"]["accession"]
             result_ids.append(accesion)
-            if write_on_sdout == True:
+            if write_on_sdout:
                 sys.stdout.write(accesion + "\n")
             if save_to_file:
                 with open(type + "_accessions_" + "_".join(re.split("\s+", keyword)) + ".csv", "a+") as f:
@@ -277,5 +276,135 @@ def browse_by_type(type: str, keyword: str = "", write_on_sdout: bool = True, sa
         sys.exit()
     else:
         return result_ids
+    
+
+def browse_proteomes(organism: str, write_on_sdout: bool = True, save_to_file: bool = False):
+    """
+    Browse proteomes from the InterPro database for a specific organism.
+
+    Argument organism is a name of the organism to browse with.
+
+    Returns list of proteome accession numbers that are matching the request.
+    """
+    organism_string = "%20".join(re.split("\s+", organism.lower().strip()))
+    BASE_URL = f"https://www.ebi.ac.uk:443/interpro/api/proteome/uniprot/entry/InterPro/?search={organism_string}&page_size=200"
+
+    context = ssl._create_unverified_context()
+
+    next = BASE_URL
+    last_page = False #
+    result_ids = []
+    while next:
+        try:
+            req = request.Request(next)
+            res = request.urlopen(req, context = context)
+            if res.status == 408:
+                sleep(61)
+                continue
+            elif res.status == 204:
+               break
+            payload = json.loads(res.read().decode())
+            next = payload["next"]
+            attempts = 0
+            if not next: #
+                last_page = True #
+        except HTTPError as e:
+            if e.code == 400:
+                sleep(61)
+                continue
+            else:
+                if attempts < 3:
+                   attempts += 1
+                   sleep(61)
+                   continue
+                else:
+                    sys.stderr.write("LAST URL: " + next)
+                    raise e
+        except Exception as e:
+            sys.stderr.write("LAST URL: " + next)
+            raise e
+        for i, item in enumerate(payload["results"]):
+            accesion = item["metadata"]["accession"]
+            result_ids.append(accesion)
+            if write_on_sdout:
+                sys.stdout.write(accesion + "\n")
+            if save_to_file:
+                with open("proteome_accessions_" + "_".join(re.split("\s+", organism)) + ".csv", "a+") as f:
+                    f.write(accesion + "\n")
+        
+        if next:
+            sleep(1)
+    
+    if result_ids == []:
+        print("There is no data associated with this request.")
+        sys.exit()
+    else:
+        return result_ids
+    
+def browse_by_database(database: str, type: str, keyword: str = "", write_on_sdout: bool = True, save_to_file: bool = False):
+    """
+    Browse entries from selected database based on a specific type and keyword.
 
     
+    Argument database is the name of the database to browse (e.g., UniProt, InterPro), type is the type of entry to browse (e.g., family, domain), and keyword (optional argument) is a keyword used to filter the entries.
+
+    Returns list of accession numbers of selected type from selected database that are matching the request.
+    """
+    if keyword != "":
+        keyword_string = "%20".join(re.split("\s+", keyword.lower().strip()))
+        BASE_URL = f"https://www.ebi.ac.uk:443/interpro/api/entry/{database}/?type={type}&search={keyword}&page_size=200"
+    
+    else:
+        BASE_URL = f"https://www.ebi.ac.uk:443/interpro/api/entry/{database}/?type={type}&page_size=200"
+
+    context = ssl._create_unverified_context()
+
+    next = BASE_URL
+    last_page = False #
+    result_ids = []
+    while next:
+        try:
+            req = request.Request(next)
+            res = request.urlopen(req, context = context)
+            if res.status == 408:
+                sleep(61)
+                continue
+            elif res.status == 204:
+               break
+            payload = json.loads(res.read().decode())
+            next = payload["next"]
+            attempts = 0
+            if not next: #
+                last_page = True #
+        except HTTPError as e:
+            if e.code == 400:
+                sleep(61)
+                continue
+            else:
+                if attempts < 3:
+                   attempts += 1
+                   sleep(61)
+                   continue
+                else:
+                    sys.stderr.write("LAST URL: " + next)
+                    raise e
+        except Exception as e:
+            sys.stderr.write("LAST URL: " + next)
+            raise e
+        for i, item in enumerate(payload["results"]):
+            accesion = item["metadata"]["accession"]
+            result_ids.append(accesion)
+            if write_on_sdout:
+                sys.stdout.write(accesion + "\n")
+            if save_to_file:
+                with open(database + "_" + type + "_accessions_" + "_".join(re.split("\s+", keyword)) + ".csv", "a+") as f:
+                    f.write(accesion + "\n")
+        
+        if next:
+            sleep(1)
+    
+    if result_ids == []:
+        print("There is no data associated with this request.")
+        sys.exit()
+    else:
+        return result_ids
